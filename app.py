0bd0cf5e376e7193e3513f9e016ffd4f5d5f43a5
Riv33R <dimageht69@gmail.com>
2024-04-24 21:22:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,33 +4,44 @@ import pandas as pd
 
 app = Flask(__name__)
 
+
 def read_schedule(file_path):
     # Считываем данные из файла Excel
-    df = pd.read_excel(file_path, engine='openpyxl')
+    df = pd.read_excel(file_path, engine="openpyxl")
 
-    # Определение текущего дня недели
-    days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
-    today = days[datetime.now().weekday()]
-    
-    # Подготовка данных для текущего дня
-    day_column = today
-    time_column = "Время" if today == "Понедельник" else f"Время.{days.index(today)}"
-    schedule_today = df[[day_column, time_column]]
-    
-    # Переименовываем столбцы для унификации
-    schedule_today.columns = ['Предмет', 'Время']
-    
-    # Возвращаем данные за текущий день
-    return schedule_today
+    # Подготовка данных для каждого дня недели
+    days = [
+        "Понедельник",
+        "Вторник",
+        "Среда",
+        "Четверг",
+        "Пятница",
+        "Суббота",
+        "Воскресенье",
+    ]
+    week_schedule = {}
+    for index, day in enumerate(days):
+        day_column = day
+        time_column = "Время" if index == 0 else f"Время.{index}"
+        day_schedule = df[[day_column, time_column]].dropna()
+        day_schedule.columns = ["Предмет", "Время"]
+        week_schedule[day] = day_schedule.to_dict(
+            orient="records"
+        )  # Преобразуем DataFrame в список словарей
 
-@app.route('/')
+    return week_schedule
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/schedule')
+
+@app.route("/schedule")
 def schedule():
-    data = read_schedule('C:\\Users\\d.ivanov\\Documents\\shedule.xlsx')
-    return jsonify(data.to_dict(orient='records'))
+    data = read_schedule("C:\\Users\\d.ivanov\\Documents\\shedule.xlsx")
+    return jsonify(data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
